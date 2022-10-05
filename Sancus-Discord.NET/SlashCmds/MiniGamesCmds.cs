4175d37a470522a0c2c6cf97b1a402e8cd2dcbdb
solar <bmacaulay404@gmail.com>
2022-10-05 13:50:28 +0100

    Started creating a game model

---
+++ Sancus-Discord.NET/SlashCmds/MiniGamesCmds.cs
@@ -0,0 +1,25 @@
+using Discord.Interactions;
+using Discord.WebSocket;
+
+namespace Sancus_Discord.NET.SlashCmds;
+
+public class MiniGamesCmds : InteractionModuleBase
+{
+    private readonly InteractionService _interaction;
+    private Random _random = new Random();
+    
+    public MiniGamesCmds(InteractionService interaction)
+    {
+        _interaction = interaction;
+    }
+
+    [SlashCommand("rock_paper_scissors", "Play a classic game of Rock Paper Scissors with the bot")]
+    public async Task RockPaperScissors(
+        [Choice("Rock", "r"), Choice("Paper", "p"), Choice("Scissors", "s")] string option)
+    {
+        string[] choices = new[] { "r", "p", "s" };
+        string botChoice = choices[_random.Next(choices.Length)];
+
+        await ReplyAsync(botChoice);
+    }
+}
