25eac856949467a2d69334e20ca1368cb3421166
Solar <bmacaulay404@gmail.com>
2022-10-06 23:29:04 +0100

    Sorted the winning and sorted some comments

--- Sancus-Discord.NET/SlashCmds/MiniGamesCmds.cs
+++ Sancus-Discord.NET/SlashCmds/MiniGamesCmds.cs
@@ -17,9 +17,29 @@ public class MiniGamesCmds : InteractionModuleBase
     public async Task RockPaperScissors(
         [Choice("Rock", "r"), Choice("Paper", "p"), Choice("Scissors", "s")] string option)
     {
+        // Sets arrays for each object 
         string[] choices = new[] { "r", "p", "s" };
-        string botChoice = choices[_random.Next(choices.Length)];
+        string[] choicesName = new[] { "Rock", "Paper", "Scissors" };
+        int randomNumber = _random.Next(choices.Length);
+        string botChoice = choices[randomNumber];
+        string botWord = choicesName[randomNumber];
 
-        await RespondAsync(botChoice);
+        // If user wins
+        if (((botChoice == "r") && (option == "p")) ||
+            (botChoice == "p" && option == "s") ||
+            (botChoice == "s" && option == "r"))
+        {
+            await RespondAsync($"Bot choose {botWord}\nYOU WON!!");
+        }
+        // If user and bot tie
+        else if (botChoice == option)
+        {
+            await RespondAsync($"Bot choose {botWord}\nYou have the same word, tie.");
+        }
+        //  If user loses
+        else
+        {
+            await RespondAsync($"Bot choose {botWord}\nYou lost, :(");
+        }
     }
 }
