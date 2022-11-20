dcd9d1f5b887b2e1ca9201edfaa2a878512b6886
Solar <bmacaulay4843@gmail.com>
2022-11-20 22:31:15 +0000

    Basic event listener for guild events

--- Sancus-Discord.NET/Events/EventManager.cs
+++ Sancus-Discord.NET/Events/EventManager.cs
@@ -1,23 +1,89 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Bson;
+using MongoDbService;
+using Sancus_Discord.NET.Elements;
+using Sancus_Discord.NET.Models;
 
 namespace Sancus_Discord.NET.Events;
 
-public static class EventManager
+public class EventManager
 {
-    
-    public static async Task UserJoined(SocketGuildUser user)
-    {
-        var acceptGuilds = new List<ulong>()
-        {
-            780211278614364160
-        };
+    private readonly IEntityBaseRepository<GuildSettings> _guildSettings;
 
-        if (!acceptGuilds.Contains(user.Guild.Id))
-            return;
-        
-        if (user.Guild.Id == 780211278614364160)
+    public EventManager(IServiceProvider services)
+    {
+        var scope = services.CreateScope();
+        var provider = scope.ServiceProvider;
+        _guildSettings = provider.GetRequiredService<IEntityBaseRepository<GuildSettings>>();
+    }
+
+    public async Task UserJoined(SocketGuildUser user)
+    {
+        var guildSetting = _guildSettings.SearchFor(x => x.GuildId == user.Guild.Id)[0];
+        Console.WriteLine(guildSetting.GuildId);
+
+        if (guildSetting is not { UserJoinLog: true }) return;
+
+        var message = new LunarEmbed()
         {
-            throw new NotImplementedException();
-        }
+            Author = new EmbedAuthorBuilder()
+            {
+                IconUrl = user.GetGuildAvatarUrl(),
+                Name = user.Guild.Name
+            },
+            Title = $"Welcome {user.Nickname} to the server",
+            ThumbnailUrl = user.GetDisplayAvatarUrl()
+        };
+        Console.WriteLine(guildSetting.UserJoinedLogChannel);
+        var logChannel = user.Guild.Channels.Where(x => x.Id == guildSetting.UserJoinedLogChannel)
+            .First() as SocketTextChannel;
+        Console.WriteLine(logChannel.Name);
+        if (logChannel is not null)
+            await logChannel.SendMessageAsync(embed: message.Build());
+    }
+
+    public async Task MessageEdit(Cacheable<IMessage, ulong> before, SocketMessage after,
+        ISocketMessageChannel channel)
+    {
+        var guildChannel = channel as SocketGuildChannel;
+        var guildSetting = _guildSettings.SearchFor(x => x.GuildId == guildChannel.Guild.Id)[0];
+
+        if (guildSetting is not { MessageEditLog: true }) return;
+
+        var beforeMessageContent = await before.GetOrDownloadAsync();
+        Console.WriteLine(before.Id == after.Id);
+
+        var returnMessage = new LunarEmbed()
+        {
+            Author = new EmbedAuthorBuilder()
+            {
+                IconUrl = after.Author.GetAvatarUrl(),
+                Name = after.Author.Username
+            },
+            Title = $"A message has been edited, {after.Id}",
+            Fields = new List<EmbedFieldBuilder>()
+            {
+                new EmbedFieldBuilder()
+                {
+                    Name = "Old message",
+                    Value = $"{beforeMessageContent}",
+                    IsInline = false
+                },
+                // TODO fix the after message content
+                // new EmbedFieldBuilder()
+                // {
+                //     Name = "Edited message",
+                //     Value = $"{after}",
+                //     IsInline = false
+                // }
+            }
+        };
+        var logChannel = guildChannel.Guild.Channels.Where(x => x.Id == guildSetting.MessageEditLogChannel)
+            .First() as SocketTextChannel;
+
+        if (logChannel is not null)
+            await logChannel.SendMessageAsync(embed: returnMessage.Build());
     }
 }
