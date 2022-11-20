caa2826c707bde516a46a02a4dae754d4b32d9d5
Solar <bmacaulay4843@gmail.com>
2022-11-20 22:30:49 +0000

    Created a basic GuildSettings model for use with the guild methods

---
+++ Sancus-Discord.NET/Models/GuildSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDbService.Models;
+
+namespace Sancus_Discord.NET.Models;
+
+public class GuildSettings : EntityBase
+{
+    [Required]
+    public ulong GuildId { get; set; }
+
+    public bool UserJoinLog { get; set; } = false;
+    public ulong? UserJoinedLogChannel { get; set; }
+
+    public bool MessageEditLog { get; set; } = false;
+    public ulong? MessageEditLogChannel { get; set; }
+}
