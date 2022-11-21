ffbf380e1ada76dee81e4a8ccf5e17bbba44965f
Solar <bmacaulay4843@gmail.com>
2022-11-21 21:50:47 +0000

    Added an icon to the lunar embed message
    This will be later changed to use the Lunar Dev CDN on the VPS

--- Sancus-Discord.NET/Elements/LunarEmbed.cs
+++ Sancus-Discord.NET/Elements/LunarEmbed.cs
@@ -7,12 +7,14 @@ public class LunarEmbed : EmbedBuilder
     public static Color SuccessColor = new Color(0x33FF7D);
     public static Color ErrorColor = new Color(0xF64545);
     public static Color InfoColor = new Color(0x4BDCE9);
-    
+
     public LunarEmbed()
     {
         Footer = new EmbedFooterBuilder()
         {
-            Text = "By Lunar Development"
+            Text = "By Lunar Development",
+            // TODO eventually switch this to use the Lunar CDN for the icon
+            IconUrl = "https://cdn.discordapp.com/attachments/789247201678327838/879862055404965938/stage_1629845776.jpeg"
         };
         Timestamp = DateTimeOffset.Now;
     }
