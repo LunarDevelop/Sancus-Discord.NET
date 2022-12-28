889d9c4307a6115f7daf16e572b610c86097f332
Solar <solar+git@lunar-dev.com>
2022-12-28 00:51:41 +0000

    Added some utilities functions

---
+++ Sancus-Discord.NET/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+using MongoDbService;
+using Sancus_Discord.NET.Models;
+
+namespace Sancus_Discord.NET;
+
+public class Utilities
+{
+    private readonly IEntityBaseRepository<GuildSettings> _guildSettings;
+
+    public Utilities(IEntityBaseRepository<GuildSettings> guildSettings)
+    {
+        _guildSettings = guildSettings;
+    }
+
+    public GuildSettings GetGuildSettings(IGuild guild)
+    {
+        var settingsList = _guildSettings.SearchFor(x => x.GuildId == guild.Id).ToList();
+        if (settingsList.Count == 0)
+            return new GuildSettings()
+            {
+                GuildId = guild.Id
+            };
+        return settingsList[0];
+    }
+}
