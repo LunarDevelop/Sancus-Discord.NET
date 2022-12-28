1340aea0f3925183effe72a1896dc719ba2fc287
Solar <solar+git@lunar-dev.com>
2022-12-28 00:53:58 +0000

    Created a StandardEnums with a true or false choice

---
+++ Sancus-Discord.NET/SlashCmds/StandardEnums.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Discord.Interactions;
+
+namespace Sancus_Discord.NET.SlashCmds;
+
+public static class StandardEnums {
+
+    public enum TrueFalseChoice
+    {
+        [ChoiceDisplay("False"), ]
+        False,
+        [ChoiceDisplay("True")]
+        True
+    }
+
+}
+
+public static class EnumUtils
+{
+    /// <summary>
+    /// Converts a True, False enum to bool objects
+    /// </summary>
+    /// <returns></returns>
+    public static bool Value(this StandardEnums.TrueFalseChoice value)
+    {
+        return value switch
+        {
+            StandardEnums.TrueFalseChoice.True => true,
+            StandardEnums.TrueFalseChoice.False => false,
+            _ => throw new ArgumentOutOfRangeException("value")
+        };
+    }
+}
